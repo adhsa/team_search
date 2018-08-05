d9fe9aa19e7df517bf641a57ea6f9ec0cbe3abe7
adhsa <asatna13@gmail.com>
2018-08-05 11:43:54 +0200

    Add files via upload
    Deploy update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn team_search:app --log-file=-
--- team_search.py
+++ team_search.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template, request, jsonify
 from elasticsearch import Elasticsearch
+from flask_cors import CORS
+
 
 # Stuff for flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hello'
+CORS(app)
 
-# Connect to elasticsearch
+
 client = Elasticsearch(
-    'https://username:password@363516e916bf46eea1aaa035058f02d7.eu-west-1.aws.found.io:9243')
+    'https://elastic:kflECKolmJVOLN4V9EPYEmsu@363516e916bf46eea1aaa035058f02d7.eu-west-1.aws.found.io:9243',
+    http_auth=('elastic', 'kflECKolmJVOLN4V9EPYEmsu'),
+    verify_certs=False
+)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        keyword = request.form['team_query']
+        keyword = request.json['team_query']
 
         query = {
-            'size': 20,
-            'query': {
-                'match': {
-                    'text': keyword
-                }
-            }
+           'size': 20,
+           'query': {
+               'match': {
+                   'text': keyword
+               }
+           }
         }
         res = client.search(index='team_search', body=query)
         data = [{'link': r['_id'], 'score': r['_score'], 'team': r['_id'].split('igem.org/Team:')[-1]} for r in res['hits']['hits']]
